956027b247523a0a3770550e882eecf18758522e
Scoofszlo <scoofszlo@gmail.com>
2024-08-18 21:54:37 +0800

    fix: Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,9 @@
-set PYTHONPATH=%PYTHONPATH%;%~dp0src
-python -m src.receiptmanager_scoofszlo
+@REM Creates virtual environment or some sort of isolated space so that 
+@REM installation of external dependencies will only be installed in this
+@REM folder
+IF NOT EXIST .venv (
+    python -m venv .venv
+)
+
+@REM Runs the program
+python -m src.receiptmanager
